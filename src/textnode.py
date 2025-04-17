eedc5afe7c4c9b3726edba7f80a91a1b007be0df
birbstorm <russellandy98@gmail.com>
2025-04-17 19:04:37 +0200

    converted text nodes to HTML and added test cases

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from htmlnode import LeafNode
+
 
 class TextType(Enum):
     TEXT = "text"
@@ -23,3 +25,22 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(node):
+    result = ''
+    match node.TextType:
+        case TextType.TEXT:
+            return LeafNode(None, node.text)
+        case TextType.LINK:
+            return LeafNode("a", node.text, {"href": node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": node.url, "alt": node.text})
+        case TextType.BOLD:
+            return LeafNode("b", node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", node.text)
+        case TextType.CODE:
+            return LeafNode("code", node.text)
+        case _:
+            raise ValueError(f"invalid type: {node.TextType}")  
+    return result
